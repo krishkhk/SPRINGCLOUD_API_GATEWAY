d461c202ce0d22240371fafbde25a314723bf446
hk21398 <hk21398@ojas-dd539.corp.ojas-it.com>
2022-09-05 15:25:44 +0530

    1st commit

---
+++ SPIRNG_CLOUD_GATEWAY/src/main/java/com/srpincloud/gateway/SpirngCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.srpincloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ SPIRNG_CLOUD_GATEWAY/src/test/java/com/srpincloud/gateway/SpirngCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srpincloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
